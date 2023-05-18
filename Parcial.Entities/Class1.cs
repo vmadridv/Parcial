0251c63c54aac15461084a2b43599f804dc0200b
Madrid <Madrid@madrid.com>
2023-05-17 18:27:59 -0600

    Creacion de clases

--- Parcial.Entities/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Parcial.Entities
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Parcial.Entities/Vehiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial.Entities
+{
+    public class Vehiculo
+    {
+        [Key]
+        public int idVehiculo { get; set; }
+        [MaxLength(30)]
+        [Required]
+
+        public String Marca { get; set; }
+        [Required]
+
+        public String Modelo { get; set; }
+
+        public String Anio { get; set; }
+
+        public int IdSucursal { get; set; }
+        
+
+        public virtual Sucursal Sucursales { get; set;  }
+    }
+}
